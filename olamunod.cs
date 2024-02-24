0ad82df294bab2618055424735d666c48ff199d7
Marinaldo Silva <marinaldo12@hotmail.com>
2024-02-24 11:29:41 -0300

    upando primeiros arquivos em csharp

---
+++ olamunod.cs
@@ -0,0 +1,13 @@
+using system;
+
+namespace aied{
+	class Teste{
+	
+		stacic void Main(String[] args){
+			console.WriteLine("Teste em Csharp");
+				
+		
+		} 		
+
+	}	
+}
---
+++ teste.cs
@@ -0,0 +1,9 @@
+using System;
+
+namespace aied{
+	class Teste{
+		static void Main(string[] args){
+			Console.WriteLine("Teste em Csharp");
+		} 		
+	}
+}
